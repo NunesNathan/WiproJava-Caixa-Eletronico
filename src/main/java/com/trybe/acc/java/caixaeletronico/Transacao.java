513a3abaa66d31ce6c6af18f44d8f9d71d271d16
NunesNathan <nunesxd7@hotmail.com>
2022-09-05 12:05:09 -0300

    feat: tdd transacao works

--- src/main/java/com/trybe/acc/java/caixaeletronico/Transacao.java
+++ src/main/java/com/trybe/acc/java/caixaeletronico/Transacao.java
@@ -1,6 +1,37 @@
 package com.trybe.acc.java.caixaeletronico;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Transacao {
+  private double quantia;
+  private String instante;
+  private String descricao;
+  private Conta conta;
+
+  public Transacao(double quantia, String descricao) {
+    this.quantia = quantia;
+    this.descricao = descricao;
+
+    this.instante = this.retornarInstante();
+  }
+
+  public double getQuantia() {
+    return this.quantia;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public String retornarResumoTransacao() {
+    return this.instante + " -------- " + this.descricao + ": R$ " + this.quantia + " +";
+  }
+
+  public String retornarInstante() {
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+    return dtf.format(LocalDateTime.now());
+  }
 
 }
 
--- src/test/java/com/trybe/acc/java/caixaeletronico/TransacaoTest.java
+++ src/test/java/com/trybe/acc/java/caixaeletronico/TransacaoTest.java
@@ -1,8 +1,12 @@
 package com.trybe.acc.java.caixaeletronico;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @DisplayName("Testes dos métodos da classe Transacao")
 class TransacaoTest {
@@ -11,30 +15,32 @@ class TransacaoTest {
   @Test
   @DisplayName("21 - Testa o método construtor da classe Transacao.")
   void construtorTest() {
-    fail("Não implementado");
-
+    Transacao transacao = new Transacao(12.00, "transferencia recebida");
+    assertEquals(transacao.getDescricao(), "transferencia recebida");
+    assertEquals(transacao.getQuantia(), 12.00);
   }
 
 
   @Test
   @DisplayName("22 - Testa o método Getter do atributo quantia.")
   void getQuantiaTest() {
-    fail("Não implementado");
-
+    Transacao transacao = new Transacao(12.00, "transferencia recebida");
+    assertEquals(transacao.getQuantia(), 12.00);
   }
 
   @Test
   @DisplayName("23 - Testa o método retornar resumo transacao.")
   void retornarResumoTransacaoTest() {
-    fail("Não implementado");
-
+    Transacao transacao = new Transacao(12.00, "transferencia recebida");
+    assertEquals(transacao.retornarResumoTransacao(), transacao.retornarInstante() + " -------- transferencia recebida: R$ 12.0 +");
   }
 
   @Test
   @DisplayName("24 - Testa o método instante está gerando o instante corretamente.")
   void retornarInstanteTest() {
-    fail("Não implementado");
-
+    Transacao transacao = new Transacao(12.00, "transferencia recebida");
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+    assertEquals(transacao.retornarInstante(), dtf.format(LocalDateTime.now()));
   }
 
 }

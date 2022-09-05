e20a8441e279769ea0edc3e89ac28fee5a2d7bf3
NunesNathan <nunesxd7@hotmail.com>
2022-09-05 13:02:41 -0300

    feat: tdd conta works

--- src/main/java/com/trybe/acc/java/caixaeletronico/Conta.java
+++ src/main/java/com/trybe/acc/java/caixaeletronico/Conta.java
@@ -1,6 +1,55 @@
 package com.trybe.acc.java.caixaeletronico;
 
+import java.util.ArrayList;
+
 public class Conta {
+  private String tipoConta;
+  private String idConta;
+  private PessoaCliente pessoaCliente;
+  private ArrayList<Transacao> transacoes = new ArrayList<Transacao>();
 
+  public Conta(String tipoConta, PessoaCliente pessoaCliente, Banco banco) {
+    this.tipoConta = tipoConta;
+    this.pessoaCliente = pessoaCliente;
+    this.idConta = banco.gerarNumeroNovaConta();
+  }
 
+  public void adicionarTransacao(double quantia, String descricao) {
+    transacoes.add(new Transacao(quantia, descricao));
+  }
+
+  public double retornarSaldo() {
+    double total = 0;
+    for (Transacao transacao : transacoes) {
+      total += transacao.getQuantia();
+    }
+
+    return total;
+  }
+
+  public String getIdConta() {
+    return this.idConta;
+  }
+
+  public PessoaCliente getPessoaCliente() {
+    return this.pessoaCliente;
+  }
+
+  public int quantidadeTransacoes() {
+    return this.transacoes.size();
+  }
+
+  public String getTipoConta() {
+    return this.tipoConta;
+  }
+
+  public String retornarResumoConta() {
+    return this.idConta + " : " + "R$" + this.retornarSaldo() + " : " + this.tipoConta;
+  }
+
+  public void retornarExtrato() {
+    for (int i = 0; i <= transacoes.size(); i++) {
+      System.out.println(String.valueOf(i + 1) + ") " + transacoes.get(i).retornarResumoTransacao());
+    }
+  }
 }
--- src/test/java/com/trybe/acc/java/caixaeletronico/ContaTest.java
+++ src/test/java/com/trybe/acc/java/caixaeletronico/ContaTest.java
@@ -1,8 +1,9 @@
 package com.trybe.acc.java.caixaeletronico;
 
-import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Teste da classe Conta")
 class ContaTest {
@@ -10,21 +11,33 @@ class ContaTest {
   @Test
   @DisplayName("6 - Testa o construtor da classe conta.")
   void construtorTest() {
-    fail("Não implementado");
+    Conta conta = new Conta("Poupança", new PessoaCliente("Adalto Alto", "333.444.111-25", "6789"), new Banco());
+
+    assertEquals(conta.getTipoConta(), "Poupança");
+    assertEquals(conta.getIdConta().length(), 10);
+    assertTrue(conta.getPessoaCliente() instanceof PessoaCliente);
   }
 
   @Test
   @DisplayName("7 - Testa o método adicionar transação e retornar saldo da conta.")
   void adicionarTransacaoTestRetornarSaldoTest() {
-    fail("Não implementado");
+    Conta conta = new Conta("Poupança", new PessoaCliente("Adalto Alto", "333.444.111-25", "6789"), new Banco());
+    conta.adicionarTransacao(12.00, "Transferencia recebida");
 
+    assertTrue(conta.quantidadeTransacoes() == 1);
+
+    conta.adicionarTransacao(13.10, "Transferencia recebida");
+
+    assertTrue(conta.retornarSaldo() == 25.10);
   }
 
   @Test
   @DisplayName("8 - Testa o método retornar resumo está retornando uma string com os valores corretamente.")
   void retornarResumoContaTest() {
-    fail("Não implementado");
+    Conta conta = new Conta("Poupança", new PessoaCliente("Adalto Alto", "333.444.111-25", "6789"), new Banco());
+    conta.adicionarTransacao(12.00, "Transferencia recebida");
 
+    assertEquals(conta.retornarResumoConta(), String.format("%s : R$12.0 : Poupança", conta.getIdConta()));
   }
 
   @Test
@@ -37,14 +50,17 @@ class ContaTest {
   @Test
   @DisplayName("10 - Testa o método Getter do atributo idConta está retornando.")
   void getIdContaTest() {
-    fail("Não implementado");
+    Conta conta = new Conta("Poupança", new PessoaCliente("Adalto Alto", "333.444.111-25", "6789"), new Banco());
 
+    assertEquals(conta.getIdConta().length(), 10);
   }
 
   @Test
   @DisplayName("11 - Testa o método método Getter do atributo pessoaCliente está retornando.")
   void getPessoaClienteTest() {
-    fail("Não implementado");
+    Conta conta = new Conta("Poupança", new PessoaCliente("Adalto Alto", "333.444.111-25", "6789"), new Banco());
+
+    assertTrue(conta.getPessoaCliente() instanceof PessoaCliente);
   }
 
 }
